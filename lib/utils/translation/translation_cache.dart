5766cee9c6218e2da3b0a5b92dd954889c84a018
USER <105976728+exp-3@users.noreply.github.com>
2026-09-03 02:27:15 +0800

    chore(l10n): remove obsolete translation keys

--- lib/utils/translation/translation_cache.dart
+++ lib/utils/translation/translation_cache.dart
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2026 OMF Project
-// SPDX-FileCopyrightText: 2019-Present Contributors to FluffyChat
+// SPDX-FileCopyrightText: 2026 The Oh-My-FluffyChat Project
+// SPDX-FileCopyrightText: 2019-Present Contributors to FluffyChat and Oh-My-FluffyChat
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
